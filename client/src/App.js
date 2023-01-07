893e3f0ee9e6d8f86709cdbcf1196639d6f2ff2b
Rachit Sharma <rachit2428@Rachits-MacBook-Air.local>
2023-01-07 17:25:00 +0530

    Added PostList Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PostCreate from './PostCreate/PostCreate';
+import PostList from './PostList/PostList';
 
 const App = ()=>{
     return(
         <div className='container'>
             <h2>Create Posts</h2>
             <PostCreate />
+            <hr/>
+            <h2>Posts</h2>
+            <PostList/>
         </div>
     )
 };
